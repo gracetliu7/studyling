e8dab359a131fe2bbd17e702250271da8efd6def
Grace Liu <gracetliu7@gmail.com>
2025-12-25 12:56:03 -0800

    I don't even know what the problem isanymore

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   root: 'public',
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'public/index.html'),
